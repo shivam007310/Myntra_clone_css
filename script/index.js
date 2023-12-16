f4deb275b2d929180835b3a1e0215867f226352b
Shivam Rana <shivamrana704.sr@gmail.com>
2023-12-16 21:07:18 +0530

    js-commit

--- script/index.js
+++ script/index.js
@@ -1,12 +1,24 @@
-let bagArray=[];
-function addToBag(itemId){
-    bagArray.push(itemId);
-   
+let bagArray = [];
+displayHomepage();
+displayBagicon();
+function addToBag(itemId) {
+  bagArray.push(itemId);
+  displayBagicon();
 }
-let itemsContainerElement = document.querySelector(".items-container");
-let innerHtml = '';
-items.forEach(item => {
-  innerHtml += `
+function displayBagicon() {
+  let bagElementCount = document.querySelector(".bag-item-count");
+  if (bagArray.length > 0) {
+    bagElementCount.style.visibility = "visible";
+    bagElementCount.innerText = bagArray.length;
+  } else {
+    bagElementCount.style.visibility = "hidden";
+  }
+}
+function displayHomepage() {
+  let itemsContainerElement = document.querySelector(".items-container");
+  let innerHtml = "";
+  items.forEach((item) => {
+    innerHtml += `
   <div class="item-container">
         <img class="item-image" src="${item.image}" alt="alt text" />
         <div class="rating">${item.rating.star}⭐ | ${item.rating.review}</div>
@@ -19,5 +31,6 @@ items.forEach(item => {
         </div>
         <button class="btn-add-bag" onclick="addToBag(${item.ID})" >add to bag</button>
     </div>`;
-});
-itemsContainerElement.innerHTML = innerHtml;
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
